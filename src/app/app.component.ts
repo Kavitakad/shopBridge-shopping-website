549a52725966d8e8839e6bf537bd661ab1a7939d
kavita <kavita1.kad@gmail.com>
2022-05-14 16:04:31 +0530

    ShopBridge-shopping application

---
+++ src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component, Inject } from '@angular/core';
+import { inject } from '@angular/core/testing';
+// import { FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'myangular10';
+
+  
+  
+
+constructor(){
+  
+}
+ ngOnInit(){
+   
+ }
+ 
+
+
+}
+
+
+
+
